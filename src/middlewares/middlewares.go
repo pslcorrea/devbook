a000d7b5e1683cc745e344adf7c55c998312e350
pslcorrea <pslcorrea@gmail.com>
2026-06-18 17:20:21 -0300

    feat: validação com token jwt

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -1,13 +1,25 @@
 package middlewares
 
 import (
-	"fmt"
+	"api/src/autenticacao"
+	"api/src/respostas"
+	"log"
 	"net/http"
 )
 
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return  func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host )
+		next(w, r)
+	}
+}
+
 func Autenticar(next http.HandlerFunc) http.HandlerFunc {
 	return  func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Validando...")
+		if erro := autenticacao.ValidarToken(r); erro != nil{
+			respostas.Erro(w, http.StatusUnauthorized, erro)
+			return 
+		}
 		next(w, r)
 	}
 }
